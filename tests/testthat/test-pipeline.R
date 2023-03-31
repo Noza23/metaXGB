69cc2b30971e9f9ccddcaa29663bc0186d515a57
Noza23 <giorginozadze@Giorgis-MacBook-Pro.local>
2023-03-31 12:08:41 +0200

    Tests removed

--- tests/testthat/test-pipeline.R
+++ tests/testthat/test-pipeline.R
@@ -1,4 +1,4 @@
-test_that("Pipeline works and results are reproducible", {
-  result_default = run_config(task_id = 16, seed = 111, default = TRUE)
-  expect_equal(result_default$auc[[1]], 0.99725)
-})
+# test_that("Pipeline works and results are reproducible", {
+#   result_default = run_config(task_id = 16, seed = 111, default = TRUE)
+#   expect_equal(result_default$auc[[1]], 0.99725)
+# })
